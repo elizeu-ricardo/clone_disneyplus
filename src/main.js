39da6755c41d8e32f0b58675d7e650c237ef32b9
Elizeu Ricardo <elizeu_ricardo@yahoo.com>
2025-07-19 19:37:29 -0300

    ajuste no mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+           ocultarElementoDoHeader();
+        }else{
+            revomeElementoDoHeader();
+        }
+    })
+
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click' , function(e) {
             const abaAlvo = e.target.dataset.tabButton;
@@ -10,7 +25,28 @@ document.addEventListener('DOMContentLoaded', function() {
             e.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaRespasta);
+    }
 })
+
+function ocultarElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function revomeElementoDoHeader() {
+     const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaRespasta(element) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = element.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
